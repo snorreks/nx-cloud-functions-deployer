8567a713867525c0e67dda40316c3038c44e48ab
snorreks <snorrekstrand@hotmail.com>
2023-07-05 11:01:43 +0200

    fix: package version

--- src/types/helper-generics.ts
+++ src/types/helper-generics.ts
@@ -11,11 +11,6 @@ export type RequestFunctions = {
 	];
 };
 
-import type { Timestamp as BackendTimestamp } from '@google-cloud/firestore';
-import type { Timestamp as FrontendTimestamp } from 'firebase/firestore';
-
-type Timestamp = FrontendTimestamp | BackendTimestamp;
-
 export interface CoreData {
 	/**
 	 * The document's ID
@@ -23,24 +18,4 @@ export interface CoreData {
 	 * @see https://firebase.google.com/docs/reference/node/firebase.firestore.DocumentSnapshot#id
 	 */
 	id: string;
-	/**
-	 * The time when the document was created
-	 *
-	 * @see https://firebase.google.com/docs/reference/node/firebase.firestore.Timestamp
-	 */
-	createdAt: Timestamp;
-	/**
-	 * The time when the document was last updated
-	 *
-	 * @see https://firebase.google.com/docs/reference/node/firebase.firestore.Timestamp
-	 */
-	updatedAt?: Timestamp;
-	/**
-	 * Optional field used to sort documents.
-	 *
-	 * Recommended to use a unix timestamp of the {@link createdAt} field.
-	 *
-	 * @see https://firebase.google.com/docs/firestore/query-data/order-limit-data
-	 */
-	priority?: number;
 }
